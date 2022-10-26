0342efba1b6320200dc64e4e507d4c49b73dc810
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-26 16:50:06 -0400

    moved files and edited about index

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+import coverImage from "../../assets/cover/IMG_E2777.JPG";
 
 function About() {
 
   return (
     <section className="my-5" >
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "25%" }} alt="cover"/>
       <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
+        <strong>
+          <p>I'm a brand new Website Developer who has put his attention into this developer world. Web developing has been a complete new journey to me but also fascinating on how every single aspect operates from front end to back end.</p>
+        </strong>
       </div>
     </section>
   )
