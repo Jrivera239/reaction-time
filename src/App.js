592ee7f0316072769b886640ec9797e636293f0e
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-26 17:40:27 -0400

    css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-
+import Footer from './components/Footer';
 function App() {
 
   return (
     <div>
       <Nav />
       <main>
+        
           <About />        
       </main>
+      <Footer />
     </div>
+   
   );
 }
 
